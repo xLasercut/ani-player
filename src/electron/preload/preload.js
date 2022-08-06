4bc046758ac3c7cb806a8ac13a4a4980c25fe34a
xLasercut <tianxun.wang2004@gmail.com>
2022-08-06 20:58:55 +0100

    initial commit

---
+++ src/electron/preload/preload.js
@@ -0,0 +1,15 @@
+"use strict";
+// src/electron/preload/preload.ts
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+window.addEventListener('DOMContentLoaded', () => {
+    const replaceText = (selector, text) => {
+        const element = document.getElementById(selector);
+        if (element)
+            element.innerText = text;
+    };
+    for (const dependency of ['chrome', 'node', 'electron']) {
+        replaceText(`${dependency}-version`, process.versions[dependency]);
+    }
+});
+//# sourceMappingURL=preload.js.map
